702c0c471e149f20c1168d97b0ce10882a631e50
Leon Lusic <l.lusic@student.vu.nl>
2022-02-22 18:40:52 +0100

    i

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-with open('a_an_example.in.txt', 'r') as a_file:
+import pprint as pp
+
+with open('e_elaborate.in.txt', 'r') as a_file:
     a_example = a_file.read().splitlines()
 
 C = a_example[0]
 
-for preference in a_example:
-    
+liked = {}
+disliked = {}
 
-print(a_example)
+for liked_preference in a_example[1::2]:
+    ingredients = liked_preference.split()[1:]
+    for ingredient in ingredients:
+        if ingredient not in liked:
+            liked[ingredient] = 1
+        else:
+            liked[ingredient] += 1
+
+for disliked_preference in a_example[2::2]:
+    ingredients = disliked_preference.split()[1:]
+    for ingredient in ingredients:
+        if ingredient not in disliked:
+            disliked[ingredient] = 1
+        else:
+            disliked[ingredient] += 1
+
+pp.pprint(liked)
+pp.pprint(disliked)
